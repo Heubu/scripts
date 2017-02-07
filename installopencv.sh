76c49989b2f59f59b90738869045c055108b105a
Heubu <dr.heu@gmx.de>
2017-02-07 21:51:28 +0100

    update istalleropencv.sh
    - update opencv
    - edit buildfile

--- installopencv.sh
+++ installopencv.sh
@@ -28,19 +28,19 @@ sudo apt-get install -y python-dev python-tk python-numpy python3-dev python3-tk
 cd /Downloads
 
 #OpenCV
-wget -O opencv.zip https://github.com/Itseez/opencv/archive/3.2.0.zip
+wget -O opencv.zip https://github.com/opencv/opencv/archive/master.zip
 unzip opencv.zip
 
 #OpenCV-modules
-wget -O opencv_contrib.zip https://github.com/Itseez/opencv_contrib/archive/3.2.0.zip
+wget -O opencv_contrib.zip https://github.com/opencv/opencv_contrib/archive/master.zip
 unzip opencv_contrib.zip
 
 #Build & install OpenCV --> replace: OPENCV_EXTRA_MODULES_PATH="/home/<USERNAME>/Downloads/opencv-contrib-3.2.0/modules/ ..
 cd opencv-3.2.0/
 mkdir build
 cd build/
-cmake -D WITH_TBB=ON -D INSTALL_C_EXAMPLES=ON -D WITH_EIGEN=OFF -D BUILD_EXAMPLES=ON -D WITH_QT=ON -D WITH_OPENGL=ON -D OPENCV_EXTRA_MODULES_PATH=/home/<USERNAME>/Downloads/opencv_contrib-3.2.0/modules/ ..
-make
+cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local -D WITH_TBB=ON -D WITH_V4L=ON -D WITH_QT=ON -D INSTALL_C_EXAMPLES=ON -D INSTALL_PYTHON_EXAMPLES=ON -D OPENCV_EXTRA_MODULES_PATH=~/opencv_contrib-master/modules -D BUILD_EXAMPLES=ON -D WITH_OPENGL=ON ..make
+
 sudo make install
 
 
